3c18c93b14892a438a04d39058eccdbb5fa611e8
12123 <404930714@qq.com>
2020-04-29 11:26:32 +0800

    er86

---
+++ cmake-build-debug/CMakeFiles/my_codeforces.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_codeforces.dir/main.cpp.o"
+  "my_codeforces"
+  "my_codeforces.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_codeforces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
